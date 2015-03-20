37c4a13a2460327eda4096154f521d7fab56d4db
Alan Pinstein <alan@tourbuzz.net>
2015-03-19 21:04:49 -0500

    Initial commit.

---
+++ test/TempaliciousTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class TempaliciousTest extends PHPUnit_Framework_TestCase
+{
+    function testTempFilesCreated()
+    {
+        $t = new Tempalicous();
+        $t->setExtension('ext');
+        $tempFileWithExtension = $t->getTempfilePath();
+        $semFile = dirname($tempFileWithExtension) . '/' . basename($tempFileWithExtension);
+
+        $this->assertFileExists($semFile, "/tmp/Tempfile semaphore file doesn't exist");
+        $this->assertFileExists($tempFileWithExtension, "/tmp/Tempfile.ext doesn't exist");
+
+        $t->cleanup();
+        $this->assertFileNotExists($semFile, "/tmp/Tempfile semaphore wasn't cleaned up.");
+        $this->assertFileNotExists($tempFileWithExtension, "/tmp/Tempfile.ext wasn't cleaned up.");
+    }
+}
